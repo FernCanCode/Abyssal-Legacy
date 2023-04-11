5e33b2f2a79873fc7e62ece4327769303900ca5a
unknown <fernando.r.cansecO@gmail.com>
2023-04-10 21:03:53 -0500

    Midway Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private CapsuleCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anima;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    private bool isDashing=false;
+    private bool isDoubleJumping = false;
+    private bool isAttacking = false;
     private float xDir = 0f;
+    private int jumpCount = 0;
+    [SerializeField] private int maxJumpCount = 2;
     [SerializeField] private float moveSpeed = 12f;
     [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float dashSpeed = 35f;
+    [SerializeField] private float dashDuration =0.4f;
 
-    private enum MovementState{ idle, running, jumping, falling };
+    private float dashTimer = 0f;
+
+    private enum MovementState{ idle, running, jumping, falling, dashing, attacking };
     
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
         sprite=GetComponent<SpriteRenderer>();
         anima = GetComponent<Animator>();
     }
@@ -24,17 +38,48 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //Horizontal movement
-        //Joystick friendly
-        xDir = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xDir*moveSpeed, rb.velocity.y);
-
-        //Jumping movement
-        if(Input.GetButtonDown("Jump"))
+        if(!isDashing)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+            //Horizontal movement
+            //Joystick friendly
+            xDir = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(xDir*moveSpeed, rb.velocity.y);
 
+            //Jumping movement
+            if(Input.GetButtonDown("Jump") && (IsGrounded() || jumpCount<maxJumpCount))
+            {
+                if(IsGrounded())
+                {
+                    jumpCount=0;
+                }
+                jumpCount++;
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
+        //Dash
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            isDashing = true;
+            dashTimer=0f;
+            //reset vertical velocity
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+        if(isDashing)
+        {
+            rb.velocity = new Vector2(xDir * dashSpeed, rb.velocity.y);
+
+            dashTimer += Time.deltaTime;
+
+            if(dashTimer >= dashDuration)
+            {
+                isDashing = false;
+            }
+        }
+        if(Input.GetButtonDown("Attack") && !isAttacking)
+        {
+            isAttacking = true;
+        }
+        
         UpdateAnimationState();
         
     }
@@ -42,8 +87,18 @@ public class PlayerMovement : MonoBehaviour
     private void UpdateAnimationState()
     {
         MovementState state;
+        if(isDashing)
+        {
+            state=MovementState.dashing;
+            
+        }
+         else if(isAttacking)
+        {
+            state = MovementState.attacking;
+            isAttacking=false;
+        }
         //If moving right
-        if(xDir > 0f)
+        else if(xDir > 0f)
         {
             state = MovementState.running;
             sprite.flipX=false;
@@ -72,5 +127,10 @@ public class PlayerMovement : MonoBehaviour
         anima.SetInteger("state", (int)state);
     }
 
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    
+    }
     
 }
